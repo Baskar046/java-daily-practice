9314e48d28d3a67bf6a723f0d4434df382e8e94f
Baskar046 <sselvabaskar046@gmail.com>
2026-09-24 16:07:28 +0530

    CovertTheVowels

---
+++ String/CountVowels.java
@@ -0,0 +1,26 @@
+
+public class CountVowels {
+
+    public static void main(String[] args) {
+
+        String Input = "Automation";
+
+        char[] regex = Input.toCharArray();
+
+        int count = 0;
+
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+        for (int i = 0; i < regex.length; i++) {
+            for (int j = 0; j < vowels.length; j++) {
+                if (regex[i] == vowels[j]) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
+}
