2bc47fd5505d38f48e93d7b5c0b51905ef785f54
Baskar046 <sselvabaskar046@gmail.com>
2026-09-25 17:43:25 +0530

    Find the first non-repeated character

---
+++ String/FirstNonRepeatedChar.java
@@ -0,0 +1,38 @@
+
+public class FirstNonRepeatedChar {
+
+    public static void main(String[] args) {
+
+        String Input = "swiss";
+
+        boolean[] check = new boolean[Input.length()];
+
+        int count;
+
+        for (int i = 0; i < Input.length(); i++) {
+
+            count = 0;
+            char ch = Input.charAt(i);
+
+            if (check[i]) {
+                continue;
+            }
+
+            for (int j = 0; j < Input.length(); j++) {
+
+                char cha = Input.charAt(j);
+                if (ch == cha) {
+                    count++;
+                    check[j] = true;
+
+                }
+            }
+
+            if (count == 1) {
+                System.out.println(ch);
+                break;
+            }
+        }
+    }
+
+}
