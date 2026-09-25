4c63d587b307e250567bac243ff9f0fc68507597
Baskar046 <sselvabaskar046@gmail.com>
2026-09-25 17:40:22 +0530

    Find the first repeated character

---
+++ String/FirstRepeatedChar.java
@@ -0,0 +1,38 @@
+
+public class FirstRepeatedChar {
+
+    public static void main(String[] args) {
+
+        String Input = "baskar";
+
+        boolean[] check = new boolean[Input.length()];
+
+        int count;
+
+        for (int i = 0; i < Input.length(); i++) {
+
+            count = 0;
+            char ch = Input.charAt(i);
+
+            if (check[i]) {
+                continue;
+            }
+
+            for (int j = 0; j < Input.length(); j++) {
+
+                char cha = Input.charAt(j);
+                if (ch == cha) {
+                    count++;
+                    check[j] = true;
+
+                }
+            }
+
+            if (count > 1) {
+                System.out.println(ch);
+                break;
+            }
+        }
+    }
+
+}
